832788021a4094d1c3cfa59c19736c7de98d67d5
m2gi-cheritya <yazid.cheriti@etu.univ-grenoble-alpes.fr>
2024-10-08 15:22:24 +0200

    :recycle: Refactoring launching sripts

---
+++ javanaiseV2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+javac DONNE2/SOURCES/src/jvn/*.java
+javac -cp DONNE2/SOURCES/src/jvn/*.java  DONNE2/SOURCES/src/irc/*.java
+echo "Compilation Réussie !\n Lancée avec $1 clients en JVN2 !!!! \n\n"
+cd DONNE2/SOURCES/src
+java jvn.JvnCoordImpl & 
+sleep 4
+for ((i=1;i<=$1;i++))
+do 
+    sleep 1
+    java irc.IrcA &
+    
+done
+ 
